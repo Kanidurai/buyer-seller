27f3d4d1109306b939d2785fc0a16a209c0384f4
BharathA26 <dev@pheonixsolutions.com>
2025-01-12 17:37:25 +0530

    home

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -9,7 +9,7 @@ require("dotenv").config();
 const PORT = process.env.BE_PORT;
 
 const corsOptions = {
-  origin: "http://localhost:3000",  
+  origin: "*",  
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
